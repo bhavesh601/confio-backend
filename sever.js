7797989d360a8de6a2b561a08648bb20c4f63fb3
Bhavesh Salunke <bsalunke2022@gmail.com>
2026-04-03 22:24:41 +0530

    port changed

--- sever.js
+++ sever.js
@@ -122,3 +122,7 @@ app.post("/enquiry", (req, res) => {
   });
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+});
